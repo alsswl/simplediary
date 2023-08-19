2642b082f1ecaee1e778b926aecb1f5c1a1b1369
alsswl <alswl4223@naver.com>
2023-08-19 23:42:03 +0900

    feat : editer의 내용 저장하기

--- src/App.js
+++ src/App.js
@@ -1,38 +1,58 @@
 
+import { useRef,useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 
-const dummyList = [
-  {
-    id : 1,
-    author : "잔디",
-    content : "안녕",
-    emotion : 5,
-    created_date : new Date().getTime(),
-  },
-  {
-    id : 2,
-    author : "풀",
-    content : "안녕하이",
-    emotion : 2,
-    created_date : new Date().getTime(),
-  },
-  {
-    id : 3,
-    author : "리액트",
-    content : "안녕안녕",
-    emotion : 1,
-    created_date : new Date().getTime(),
-  },
-]
+// const dummyList = [
+//   {
+//     id : 1,
+//     author : "잔디",
+//     content : "안녕",
+//     emotion : 5,
+//     created_date : new Date().getTime(),
+//   },
+//   {
+//     id : 2,
+//     author : "풀",
+//     content : "안녕하이",
+//     emotion : 2,
+//     created_date : new Date().getTime(),
+//   },
+//   {
+//     id : 3,
+//     author : "리액트",
+//     content : "안녕안녕",
+//     emotion : 1,
+//     created_date : new Date().getTime(),
+//   },
+// ]
 
 
 const App = () =>{
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author,content,emotion) =>{
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id : dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem,...data]);//최근 것이 먼저 오도록 하기 위해 ...data보다 앞에 쓴다.
+
+  };
+  
+
   return (
     <div className="App">
-      <DiaryEditor/>
-      <DiaryList diaryList = {dummyList}/>
+      <DiaryEditor onCreate = {onCreate}/>
+      <DiaryList diaryList = {data}/>
     </div>
   );
 }
